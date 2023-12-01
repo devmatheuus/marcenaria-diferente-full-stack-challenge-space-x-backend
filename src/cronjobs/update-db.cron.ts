124162181714e8f8f078b7d4b45e9f76471edb3b
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-01 00:49:34 -0300

    fix:  cron job to update database with launch data

--- src/cronjobs/update-db.cron.ts
+++ src/cronjobs/update-db.cron.ts
@@ -1,8 +1,35 @@
+import { LaunchApiResponse } from "@/Models/Launch/launch.types";
+import { initializeMongoConnection } from "@/database/mongo";
+import { api } from "@/lib/axios";
 import cron from "node-cron";
-import { seed } from "@/database/seed";
+import LaunchModel from "@/Models/Launch/Launch";
+import mongoose from "mongoose";
 
 cron.schedule("0 9 * * *", async () => {
-    console.log("Running cron job to update database");
-    await seed();
-    console.log("Finished running cron job to update database");
+    try {
+        console.log("Running cron job to update database");
+        initializeMongoConnection();
+
+        const { data } = await api.get<LaunchApiResponse>("/");
+
+        const launchRecordExists = await LaunchModel.exists({
+            id: data.id,
+        });
+
+        if (!launchRecordExists) {
+            await LaunchModel.create(data);
+            console.log("Launch record already exists");
+            return;
+        }
+
+        console.log("Launch inserted successfully");
+    } catch (error) {
+        console.error(
+            "Error when trying to insert record into database:",
+            error
+        );
+    } finally {
+        mongoose.disconnect();
+        process.exit(0);
+    }
 });
