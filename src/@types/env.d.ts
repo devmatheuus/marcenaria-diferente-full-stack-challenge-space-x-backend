ed358199e6c52a5c29f3812c5e9d9234f0069b85
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 13:37:55 -0300

    chore: add Redis environment variables

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -4,6 +4,10 @@ declare global {
             MONGO_URI: string; // variável para conexão com o banco de dados MongoDB
             NODE_ENV: "development" | "production" | "test"; // variável para definir o ambiente de execução
             PORT: number; // variável para definir a porta de execução da aplicação
+            REDIS_KEY_GET_TOP_ROCKET_LAUNCHES: string; // variável para definir a chave de cache dos dados de estatísticas de lançamentos
+            REDIS_KEY_GET_LAUNCH_STATUS_COUNTS: string; // variável para definir a chave de cache dos dados de estatísticas de lançamentos
+            REDIS_PASSWORD: string; // variável para definir a senha de conexão com o Redis
+            REDIS_HOST: string; // variável para definir o host de conexão com o Redis
         }
     }
 }
