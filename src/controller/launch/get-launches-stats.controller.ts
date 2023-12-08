64332837aaf368195f42d54b3b6feb81248d0f43
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 12:17:08 -0300

    refactor: add comments and error handling to getLaunchesStatsController

--- src/controller/launch/get-launches-stats.controller.ts
+++ src/controller/launch/get-launches-stats.controller.ts
@@ -1,11 +1,29 @@
+import { AppError } from "@/errors/app-error";
 import { getLaunchesStatsService } from "@/services/launch/get-launches-stats.service";
 import { Request, Response } from "express";
+
+/* 
+    Chama o serviço responsável por retornar um array de objetos contendo dados dos quatro
+    foguetes com mais lançamentos.
+
+    Os dados retornados em cada objetos são:
+    * Nome do foguete;
+    * quantidade de lançamentos bem sucedidos;
+    * quantidade de lançamentos mal sucedidos;
+    * Um array contendo o ano e a quantidade de lançamentos por ano.     
+*/
 
 export const getLaunchesStatsController = async (
     req: Request,
     res: Response
 ) => {
-    const launchesData = await getLaunchesStatsService();
-
-    res.json(launchesData);
+    try {
+        const launchesData = await getLaunchesStatsService();
+        return res.json(launchesData);
+    } catch (error) {
+        throw new AppError(
+            500,
+            "Error when calling the get Launch Start service"
+        );
+    }
 };
