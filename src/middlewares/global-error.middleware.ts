4ba2b73b9703c16a62aa97a8c3ebc30ddb971b5b
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 15:52:18 -0300

    feat: add error handling for zod validation errors

--- src/middlewares/global-error.middleware.ts
+++ src/middlewares/global-error.middleware.ts
@@ -1,5 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/errors/app-error";
+import { z } from "zod";
+
+type ZodErrorType = {
+    code: string;
+    expected: string;
+    received: string;
+    path: string[];
+    message: string;
+};
+
+type ErrorMessageReturnType = {
+    [key: string]: string;
+};
 
 export const globalErrorMiddleware = async (
     error: any,
@@ -10,6 +23,24 @@ export const globalErrorMiddleware = async (
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             mensagem: error.message,
+        });
+    }
+
+    if (error instanceof z.ZodError) {
+        const message: ErrorMessageReturnType[] = [];
+
+        const errors: ZodErrorType[] = JSON.parse(error.message);
+
+        errors.forEach((err) => {
+            const errorObject = {
+                [err.path[0]]: err.message,
+            };
+
+            message.push(errorObject);
+        });
+
+        return response.status(400).json({
+            mensagem: message,
         });
     }
 
