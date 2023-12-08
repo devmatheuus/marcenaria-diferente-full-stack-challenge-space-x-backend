e9e413daf2e3422d6d0cbe9cb3af6d06d2c2957b
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 11:38:58 -0300

    refactor: adding comments to explain the purpose of each environment variable

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,9 +1,9 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            MONGO_URI: string;
-            NODE_ENV: "development" | "production" | "test";
-            PORT: number;
+            MONGO_URI: string; // variável para conexão com o banco de dados MongoDB
+            NODE_ENV: "development" | "production" | "test"; // variável para definir o ambiente de execução
+            PORT: number; // variável para definir a porta de execução da aplicação
         }
     }
 }
