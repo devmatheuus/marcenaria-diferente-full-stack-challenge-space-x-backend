12bf766c72d20ecf91b7fd053da71c26296a2cbd
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 15:53:07 -0300

    refactor: getLaunchesController to use getLaunchesSchema

--- src/controller/launch/get-launches.controller.ts
+++ src/controller/launch/get-launches.controller.ts
@@ -1,12 +1,11 @@
+import { getLaunchesSchema } from "@/schemas/get-launches.schema";
 import { getLaunchesService } from "@/services/launch/get-launches.service";
 import { Request, Response } from "express";
 
 export const getLaunchesController = async (req: Request, res: Response) => {
-    const search = req.query?.search as string;
-    const page = Number(req.query?.page) || 1;
-    const limit = Number(req.query?.limit) || 4;
+    const queryParams = getLaunchesSchema.parse(req.query);
 
-    const launches = await getLaunchesService({ search, page, limit });
+    const launches = await getLaunchesService(queryParams);
 
     return res.status(200).json({ ...launches });
 };
