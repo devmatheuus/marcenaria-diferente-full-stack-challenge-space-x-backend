430df2ee392e0dd79d0bb1a97f06bf48372fc511
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 15:55:09 -0300

    refactor: getLaunchesController to handle errors

--- src/controller/launch/get-launches.controller.ts
+++ src/controller/launch/get-launches.controller.ts
@@ -1,11 +1,16 @@
+import { AppError } from "@/errors/app-error";
 import { getLaunchesSchema } from "@/schemas/get-launches.schema";
 import { getLaunchesService } from "@/services/launch/get-launches.service";
 import { Request, Response } from "express";
 
 export const getLaunchesController = async (req: Request, res: Response) => {
-    const queryParams = getLaunchesSchema.parse(req.query);
+    try {
+        const queryParams = getLaunchesSchema.parse(req.query);
 
-    const launches = await getLaunchesService(queryParams);
+        const launches = await getLaunchesService(queryParams);
 
-    return res.status(200).json({ ...launches });
+        return res.status(200).json({ ...launches });
+    } catch (error) {
+        throw new AppError(500, "Error when calling the get Launches service");
+    }
 };
