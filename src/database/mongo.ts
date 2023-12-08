5757a8d5126e72ea799e2b4bd060e835a5dd0e06
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-08 16:23:38 -0300

    refactor: MongoDB connection code to handle disconnections and reconnect attempts

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+import { AppError } from "@/errors/app-error";
+
+if (!process.env.MONGO_URI) {
+    throw new AppError(500, "MONGO_URI not set in .env file");
+}
 
 class MongoDBConnection {
     private static instance: MongoDBConnection;
     private isConnected: boolean = false;
+    private reconnectAttempts: number = 0;
 
     private constructor() {
         this.connect();
@@ -11,18 +17,38 @@ class MongoDBConnection {
 
     private async connect() {
         try {
-            await mongoose.connect(process.env.MONGO_URI as string);
+            await mongoose.connect(process.env.MONGO_URI as string, {
+                maxPoolSize: 10,
+            });
 
             const db = mongoose.connection;
 
-            db.on("error", console.error.bind(console, "connection error:"));
+            db.on("error", () => {
+                throw new AppError(500, "Error connecting to MongoDB");
+            });
 
             db.once("open", () => {
                 console.log("MongoDB connected");
                 this.isConnected = true;
             });
+
+            db.on("disconnected", () => {
+                if (this.reconnectAttempts < 10) {
+                    console.log("MongoDB disconnected. Reconnecting...");
+
+                    setTimeout(() => {
+                        this.reconnectAttempts++;
+                        this.connect();
+                    }, 5000);
+                } else {
+                    throw new AppError(
+                        500,
+                        "MongoDB disconnected, error reconnecting!"
+                    );
+                }
+            });
         } catch (error) {
-            console.error("Error connecting to MongoDB:", error);
+            throw new AppError(500, "Error connecting to MongoDB");
         }
     }
 
